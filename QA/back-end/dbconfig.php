844214228a1404219d59026e6e84c6bfdc11ad1b
Genmar Dadivo <31000214+genmar-dadivo@users.noreply.github.com>
2022-10-30 11:26:04 +0800

    Update dbconfig.php

--- QA/back-end/dbconfig.php
+++ QA/back-end/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 class Con extends PDO {
     public function __construct() {
-        parent::__construct("mysql:host=localhost;dbname=test", "root", "12302112");
+        parent::__construct("mysql:host=localhost;dbname=test", "root", "");
     }
 }
 ?>
